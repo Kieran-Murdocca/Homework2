2f2272d60fff1061dd4005adb44d1cd4f3647bbd
KieranMurdocca <kkm5754@psu.edu>
2020-09-11 20:48:14 +0000

    changed variables

--- main.py
+++ main.py
@@ -1,22 +1,22 @@
 # Author: Kieran Murdocca kkm5754@psu.edu
 # Homework 2
 
-def getGradePoint(lg1, lg2, lg3): 
-  if lg1 == "A" or lg2 == "A" or lg3 == "A": 
+def getGradePoint(lg): 
+  if lg == "A": 
     return float(4.00)
-  elif lg1 == "A-" or lg2 == "A-" or lg3 == "A-": 
+  elif lg == "A-": 
     return float(3.67)
-  elif lg1 == "B+" or lg2 == "B+" or lg3 == "B+": 
+  elif lg == "B+": 
     return float(3.33)
-  elif lg1 == "B" or lg2 == "B" or lg3 == "B": 
+  elif lg == "B": 
     return float(3.00)
-  elif lg1 == "B-" or lg2 == "B-" or lg3 == "B-": 
+  elif lg == "B-": 
     return float(2.67)
-  elif lg1 == "C+" or lg2 == "C+" or lg3 == "C+": 
+  elif lg == "C+": 
     return float(2.33)
-  elif lg1 == "C" or lg2 == "C" or lg3 == "C": 
+  elif lg == "C": 
     return float(2.00)
-  elif lg1 == "D" or lg2 == "D" or lg3 == "D": 
+  elif lg == "D": 
     return float(1.00)
   else: 
     return float(0.00)
@@ -24,14 +24,18 @@ def getGradePoint(lg1, lg2, lg3):
 def run(): 
   lg1=input("Enter your course 1 letter grade: ")
   c1=float(input("Enter your course 1 credit: "))
-  print(f"Grade point for course 1 is {getGradePoint(lg1)}.")
+  print(f"Grade point for course 1 is: {getGradePoint(lg1)}")
+  gp1=getGradePoint(lg1)
   lg2=input("Enter your course 2 letter grade: ")
   c2=float(input("Enter your course 2 credit: "))
-  print(f"Grade point for course 2 is {getGradePoint(lg2)}.")
+  print(f"Grade point for course 2 is: {getGradePoint(lg2)}")
+  gp2=getGradePoint(lg2)
   lg3=input("Enter your course 3 letter grade: ")
   c3=float(input("Enter your course 3 credit: "))
-  print(f"Grade point for course 3 is {getGradePoint(lg3)}.")
-  print(f"Your GPA is:", (float(lg1) * float(c1) + float(lg2) * float(c2) + float(lg3) * float(c3)) / (float(c1) + float(c2) + float(c3) ))
+  print(f"Grade point for course 3 is: {getGradePoint(lg3)}")
+  gp3=getGradePoint(lg3)
+  gpa = (gp1 * c1 + gp2 * c2 + gp3 * c3) / (c1 + c2 + c3)
+  print(f"Your GPA is: {gpa}")
   
 
 if __name__ == "__main__": 
